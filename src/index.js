52fdb85318b42b67b17abfd39255976c2fde0014
nadiachayna <nadia.chayna.q@gmail.com>
2021-06-17 09:32:52 -0500

    card Validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import validator from './validator.js';
+//console.log();
 
-console.log(validator);
+let validarBoton = document.getElementById("btn");
+validarBoton.addEventListener("click", () => {
+    let inputNumero = document.getElementById("cardnumber");
+    let numero = inputNumero.value;
+    const regex = /^[0-9]*$/;
+    if (numero.length >= 12 && regex.test(numero)){
+        let tarjetaValida = validator.isValid(numero);
+        if (tarjetaValida == true){
+        let tarjetaMaskify = validator.maskify(numero);
+        alert(`la tarjeta: ${tarjetaMaskify} es válida`);
+        document.getElementById("cardnumber").value ="";
+        document.getElementById("cvv").value ="";
+        } else {
+        alert("tarjeta inválida, intente nuevamente");
+        document.getElementById("cardnumber").value ="";
+        document.getElementById("cvv").value ="";
+        }
+
+    } else {
+        alert("el número de su tarjeta debe tener min 12 digitos númericos")
+    }
+    
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,44 @@
 const validator = {
-  // ...
+  isValid: function (crediCard){
+    let arreglo = crediCard.split('').reverse().map(Number);
+    //console.log(arreglo);
+    //multiplicamos las posiciones pares x2
+    arreglo = arreglo.map((num, i) => {
+     if(i % 2 == 1) {
+        return num*2
+     } else {
+        return num;
+    }   
+});
+   //console.log(arreglo);
+    //sumamos los numeros mayores a 10
+    arreglo=arreglo.map((num) => {
+    if(num >= 10 ) {
+      return num -=9;
+    } else {
+      return num
+    }
+});
+    //los digitos totales del arreglo con el método reduce que me devolverá la suma total 
+    const numero= arreglo.reduce((acumulator, currentvalue) => {
+    return acumulator + currentvalue;   
+});
+    let resultado;
+    if (numero % 10 == 0) {
+      resultado = true;
+    } else {
+    resultado = false;
+    }
+    return resultado;
+    //console.log(numero);  
+  },
+  maskify (numero){
+    let arreglo = numero.split('').reverse().map(Number);
+    for (let i = 0; i < arreglo.length -4;i++){
+      arreglo[i] = '#';
+    }
+    return(arreglo.join(''));
+  }
 };
 
 export default validator;
