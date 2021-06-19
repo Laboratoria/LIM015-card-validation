41be1f7800fad1bdbba4283871ccb1493f388c65
nadia <nadia.chayna.q@gmail.com>
2021-06-18 21:10:23 -0500

    actualización

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
+Debe contener lo siguiente:
+
+Un título con el nombre de tu proyecto.
+Un resumen de 1 o 2 líneas de qué se trata tu proyecto.
+La imagen final de tu proyecto.
+Investigación UX:
+Explicar quiénes son los usuarios y los objetivos en relación con el producto.
+Explicar cómo el producto soluciona los problemas/necesidades de dichos usuarios.
+Luego colocarás la foto de tu primer prototipo en papel.
+Agregar un resumen del feedback recibido indicando las mejoras a realizar.
+Imagen del prototipo final.
+Tu plan de acción - los Objetivos de Aprendizaje que vas a prioritizar.
+
+
+
 # Tarjeta de crédito válida
 
 ## Índice
--- test/validator.spec.js
+++ test/validator.spec.js
@@ -2,43 +2,45 @@
 import validator from '../src/validator';
 
 describe('validator', () => {
-  it('debería ser un objeto', () => {
-    expect(typeof validator).toBe('object');
-  });
-
-  describe('validator.isValid', () => {
-    it('debería ser una función', () => {
-      expect(typeof validator.isValid).toBe('function');
+    it('debería ser un objeto', () => {
+        expect(typeof validator).toBe('object');
     });
 
-    it.skip('debería retornar true para "4083952015263"', () => {
-      // escribe aquí tu test
+    describe('validator.isValid', () => {
+        it('debería ser una función', () => {
+            expect(typeof validator.isValid).toBe('function');
+        });
+
+        it('debería retornar true para "4835081277352152"', () => {
+            expect(validator.isValid("4835081277352152")).toBe(true);
+        });
+
+        it('debería retornar true para "5431710436910147"', () => {
+            expect(validator.isValid("5431710436910147")).toBe(true);
+        });
+
+        it('debería retornar false para "9812456732456784"', () => {
+            expect(validator.isValid("9812456732456784")).toBe(false);
+        });
+
     });
 
-    it.skip('debería retornar true para "79927398713"', () => {
-      // escribe aquí tu test
+    describe('validator.maskify', () => {
+        it('debería ser una función', () => {
+            expect(typeof validator.maskify).toBe('function');
+        });
+
+        it('Debería retornar "############2152" para "4835081277352152"', () => {
+            expect(validator.maskify("4835081277352152")).toBe("############2152");
+        });
+
+        it('Debería retornar "1" para "1"', () => {
+            expect(validator.maskify("1")).toBe("1");
+        });
+
+        it('Debería retornar "######orld" para "helloworld"', () => {
+            expect(validator.maskify("helloworld")).toBe("######orld");
+        });
     });
 
-    it.skip('debería retornar false para "1234567890"', () => {
-      // escribe aquí tu test
-    });
-  });
-
-  describe('validator.maskify', () => {
-    it('debería ser una función', () => {
-      expect(typeof validator.maskify).toBe('function');
-    });
-
-    it.skip('Debería retornar "############5616" para "4556364607935616"', () => {
-      // escribe aquí tu test
-    });
-
-    it.skip('Debería retornar "1" para "1"', () => {
-      // escribe aquí tu test
-    });
-
-    it.skip('Debería retornar "######orld" para "helloworld"', () => {
-      // escribe aquí tu test
-    });
-  });
 });
